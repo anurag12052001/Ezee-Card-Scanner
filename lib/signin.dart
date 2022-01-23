923eee50643931b5f55480a389b3f67bc37d2847
LAPTOP-DRERLHOL\hp <anuragdubey447@gmail.com>
2022-01-24 03:29:12 +0530

    change

--- lib/signin.dart
+++ lib/signin.dart
@@ -8,10 +8,30 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final _formkey1 = GlobalKey<FormState>();
+  TextEditingController fullname_txt = TextEditingController();
+  TextEditingController email_txt = TextEditingController();
+  TextEditingController password_txt = TextEditingController();
+  TextEditingController confirm_txt = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    // full name
+    final fullnamefield = TextFormField(
+      controller: fullname_txt,
+      autofocus: false,
+      keyboardType: TextInputType.name,
+      onSaved: (newValue) {
+        fullname_txt.text = newValue!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        hintText: "Full Name",
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    );
     return Scaffold(
-      body: Container(),
+      body: SafeArea(child: SingleChildScrollView()),
     );
   }
 }
